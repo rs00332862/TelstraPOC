507669466ab5cc2ad567c835c394e87e8f3e04a8
Rohit Shivankar <rs00332862@techmahindra.com>
2020-05-06 13:37:16 +0530

    Created New Project 'TelstraPOC' with OS support for version iOS 12 and above Removed connection with main storyboard file as we will be developing UI with code

---
+++ TelstraPOC/TelstraPOC/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewController.swift
+//  TelstraPOC
+//
+//  Created by Rohit on 5/6/20.
+//  Copyright © 2020 Rohit. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
