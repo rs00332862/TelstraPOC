f49eeb8243333aafd403d7540cf0c82c3d9c3767
Rohit Shivankar <rs00332862@techmahindra.com>
2020-06-30 21:11:31 +0530

    Added functional test case to validate web service call

--- TelstraPOC/TelstraPOCTests/TelstraPOCTests.swift
+++ TelstraPOC/TelstraPOCTests/TelstraPOCTests.swift
@@ -11,24 +11,29 @@ import XCTest
 
 class TelstraPOCTests: XCTestCase {
 
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    //Test case to validate webservcie success call
+    func testGetCategoryDataFromWebService() {
+        let expectation = self.expectation(description: "Web Service response successful.")
+        let categoryViewModel = CategoryViewModel()
+        categoryViewModel.getCategoryData { (result) in
+            switch(result) {
+            case .success(let result):
+                XCTAssertNotNil(result)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
         }
+        self.waitForExpectations(timeout: 7, handler: nil)
     }
-
 }
